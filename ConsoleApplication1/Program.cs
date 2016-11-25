bde2b3a6b17a83eec0d647e312152fcc93eb4dc7
phelow <wpheloung@gmail.com>
2016-11-25 00:28:58 -0500

    IT's working but for the wrong reasons

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -1446,7 +1446,7 @@ namespace ConsoleApplication1
                     int turnsWithoutProgress = 0;
 
 
-                    while (nextTiles.Count > 0 && watch.ElapsedMilliseconds < 10000)
+                    while (nextTiles.Count > 0 && watch.ElapsedMilliseconds < 8000)
                     {
                         AStarBoardState current = nextTiles.Dequeue();
 
