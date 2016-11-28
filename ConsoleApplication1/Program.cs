a140c6875e3b03dc6a465d2b353167a5118ce072
phelow <wpheloung@gmail.com>
2016-11-28 17:33:13 -0500

    Attempted lockdown of bot at 12000ms

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -1456,7 +1456,7 @@ namespace ConsoleApplication1
                     //BFS search to find all safe tiles
                     int turnsWithoutProgress = 0;
 
-                    float turnTime = 14000;
+                    float turnTime = 12000;
                     while (nextTiles.Count > 0 && watch.ElapsedMilliseconds < turnTime)
                     {
                         AStarBoardState current = nextTiles.Dequeue();
